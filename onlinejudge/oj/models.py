f0ade4351642f8c2c25c3eed6736b8af4ebb9c72
mkrmayank <19bcs070@iiitdwd.ac.in>
2022-07-10 13:13:32 +0530

    front end pages

--- onlinejudge/oj/models.py
+++ onlinejudge/oj/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class question(models.Model):
+class Question(models.Model):
     question_text = models.CharField(max_length = 2000)
-    pub_date
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.question_text 
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
--- onlinejudge/oj/views.py
+++ onlinejudge/oj/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 # Create your views here.
 
+# def home(request):
+#     return render(request, 'index.html')
 
 def index(request):
-    return HttpResponse("Works going on.....")
+    return render(request, 'oj/ide.html')
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("Error!!!")
+    return render(request, 'oj/details.html', {'question':question})
